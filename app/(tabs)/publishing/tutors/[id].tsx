0079718f53b2045ba03122e34c5fd20954bf6061
piscopancer <igor.bistr01092003@gmail.com>
2024-11-23 22:22:46 +0300

    .

--- app/(tabs)/publishing/tutors/[id].tsx
+++ app/(tabs)/publishing/tutors/[id].tsx
@@ -1,17 +1,19 @@
 // import SheetSelect from '@/components/sheet-select'
-import SheetSelect from '@/components/sheet-select'
+import { BottomSheet, useBottomSheetRef } from '@/components/bottom-sheet'
 import Text from '@/components/text'
 import TextInput from '@/components/text-input'
 import { db } from '@/db'
-import { queryKeys } from '@/query'
-import { Rank, ranksInfo } from '@/tutor'
-import { capitalizeFirstLetter, cn, objectEntries } from '@/utils'
+import { qc, queryKeys } from '@/query'
+import { Rank, ranks, ranksInfo } from '@/tutor'
+import { capitalizeFirstLetter, cn, colors } from '@/utils'
+import { BottomSheetView } from '@gorhom/bottom-sheet'
 import { Prisma } from '@prisma/client'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { useLocalSearchParams, useRouter } from 'expo-router'
-import { useState } from 'react'
-import { Pressable, ScrollView } from 'react-native'
+import { useEffect } from 'react'
+import { FlatList, Pressable, ScrollView } from 'react-native'
 import { indigo, neutral } from 'tailwindcss/colors'
+import { proxy, useSnapshot } from 'valtio'
 
 async function queryTutor(id: number) {
   return db.tutor.findFirst({
@@ -20,6 +22,13 @@ async function queryTutor(id: number) {
     },
   })
 }
+
+const changedStore = proxy<Partial<{ name: string; surname: string; middlename: string; rank: Rank | null }>>({
+  name: undefined,
+  surname: undefined,
+  middlename: undefined,
+  rank: undefined,
+})
 
 async function updateTutor(updated: Prisma.TutorUpdateArgs) {
   return db.tutor.update(updated)
@@ -37,16 +46,30 @@ export default function TutorScreen() {
     onError(e) {
       console.error(e)
     },
-    async onSuccess(data) {
-      console.log('tutor updated', data)
-      await tutorQuery.refetch()
+    async onSuccess() {
+      await qc.invalidateQueries({ queryKey: queryKeys.tutors })
       router.back()
     },
   })
-  const [changedSurname, setChangedSurname] = useState<string | undefined>()
-  const [changedName, setChangedName] = useState<string | undefined>()
-  const [changedMiddlename, setChangedMiddlename] = useState<string | undefined>()
-  const [changedRank, setChangedRank] = useState<Rank | undefined>()
+  const changedSnap = useSnapshot(changedStore)
+  const rankSheetRef = useBottomSheetRef()
+
+  useEffect(() => {
+    return () => {
+      changedStore.name = undefined
+      changedStore.surname = undefined
+      changedStore.middlename = undefined
+      changedStore.rank = undefined
+    }
+  }, [])
+
+  function getRank() {
+    const rank = changedSnap.rank === undefined ? (tutorQuery.data?.rank as Rank | null) : changedSnap.rank
+    if (!rank) {
+      return null
+    }
+    return capitalizeFirstLetter(ranksInfo[rank].long)
+  }
 
   return (
     <ScrollView overScrollMode='never'>
@@ -59,7 +82,7 @@ export default function TutorScreen() {
             className='mb-4 mx-4'
             onChange={(e) => {
               const text = e.nativeEvent.text
-              setChangedSurname(text === tutorQuery.data?.surname ? undefined : text)
+              changedStore.surname = text === tutorQuery.data?.surname ? undefined : text
             }}
           />
           <Text className='mb-2 ml-9 dark:text-neutral-500'>Имя</Text>
@@ -68,7 +91,7 @@ export default function TutorScreen() {
             placeholder={tutorQuery.data.name}
             onChange={(e) => {
               const text = e.nativeEvent.text
-              setChangedName(text === tutorQuery.data?.name ? undefined : text)
+              changedStore.name = text === tutorQuery.data?.name ? undefined : text
             }}
             className='mb-4 mx-4'
           />
@@ -78,40 +101,65 @@ export default function TutorScreen() {
             placeholder={tutorQuery.data.middlename}
             onChange={(e) => {
               const text = e.nativeEvent.text
-              setChangedMiddlename(text === tutorQuery.data?.middlename ? undefined : text)
+              changedStore.middlename = text === tutorQuery.data?.middlename ? undefined : text
             }}
             className='mb-4 mx-4'
           />
           <Text className='mb-2 ml-9 dark:text-neutral-500'>Должность</Text>
-          <SheetSelect
-            options={objectEntries(ranksInfo)}
-            pressableProps={{
-              className: 'mb-12 mx-4 border rounded-md border-neutral-800 px-5 py-4 bg-neutral-950',
-              android_ripple: { color: neutral[800] },
-            }}
-            onUnselect={() => setChangedRank(undefined)}
-            renderOption={({ item: [rank, info] }, setOpen) => (
-              <Pressable
+          {/*  */}
+          <Pressable onPress={() => rankSheetRef.current?.expand()} className='border rounded-md border-neutral-800 px-5 py-4 mx-4 mb-8'>
+            <Text className={cn('text-lg', getRank() ? '' : 'dark:text-neutral-500')}>{getRank() ?? 'Не указана'}</Text>
+          </Pressable>
+          <BottomSheet ref={rankSheetRef}>
+            <BottomSheetView>
+              {/* <Pressable
+                android_ripple={{ color: colors.neutral[700] }}
                 onPress={() => {
-                  setChangedRank(rank)
-                  setOpen(false)
+                  changedStore.rank = undefined
+                  rankSheetRef.current?.close()
                 }}
-                android_ripple={{ color: neutral[700] }}
-                className='py-4'
+                className='px-6 py-4 flex-row items-center border-b border-neutral-800'
               >
-                <Text className='text-lg text-center'>{capitalizeFirstLetter(info.long)}</Text>
+                <LucideRotateCcw strokeWidth={1} className='mr-5 color-neutral-500' />
+                <Text className='text-lg'>Отменить изменения</Text>
+              </Pressable> */}
+              <Pressable
+                android_ripple={{ color: colors.neutral[700] }}
+                onPress={() => {
+                  changedStore.rank = null
+                  rankSheetRef.current?.close()
+                }}
+                className='px-6 py-4 flex-row items-center border-b border-neutral-800'
+              >
+                {/* <LucideUserRoundX strokeWidth={1} className='mr-5 color-neutral-500' /> */}
+                <Text className='text-lg'>Не указана</Text>
               </Pressable>
-            )}
-          >
-            <Text className={cn('text-lg', tutorQuery.data.rank ? '' : 'dark:text-neutral-500')}>{tutorQuery.data.rank ? capitalizeFirstLetter(ranksInfo[tutorQuery.data.rank as Rank].long) : 'Нет должности'}</Text>
-          </SheetSelect>
+              <FlatList
+                data={ranks}
+                renderItem={({ item: rank }) => (
+                  <Pressable
+                    android_ripple={{ color: colors.neutral[700] }}
+                    onPress={() => {
+                      changedStore.rank = (tutorQuery.data!.rank as Rank | null) === rank ? undefined : rank
+                      rankSheetRef.current?.close()
+                    }}
+                    className='px-6 py-4 flex-row items-center'
+                  >
+                    {/* <LucideUserRound strokeWidth={1} className='mr-5 color-neutral-500' /> */}
+                    <Text className='text-lg line-clamp-1'>{capitalizeFirstLetter(ranksInfo[rank].long)}</Text>
+                  </Pressable>
+                )}
+              />
+            </BottomSheetView>
+          </BottomSheet>
+
           <Pressable
             onPress={() => {
               if (tutorQuery.data) {
-                setChangedSurname(tutorQuery.data.surname)
-                setChangedName(tutorQuery.data.name)
-                setChangedMiddlename(tutorQuery.data.middlename)
-                setChangedRank((tutorQuery.data.rank as Rank | null) ?? undefined)
+                changedStore.name = tutorQuery.data.name
+                changedStore.surname = tutorQuery.data.surname
+                changedStore.middlename = tutorQuery.data.middlename
+                changedStore.rank = (tutorQuery.data.rank as Rank | null) ?? undefined
               }
             }}
             className='mb-4 mx-4 py-4 px-6 bg-neutral-900 rounded-md'
@@ -128,11 +176,10 @@ export default function TutorScreen() {
                   id: Number(tutorId),
                 },
                 data: {
-                  // todo - fix prisma skip resolves to undefined
-                  surname: changedSurname ?? Prisma.skip,
-                  name: changedName ?? Prisma.skip,
-                  middlename: changedMiddlename ?? Prisma.skip,
-                  rank: changedRank ?? Prisma.skip,
+                  ...(changedStore.name ? { name: changedStore.name } : {}),
+                  ...(changedStore.surname ? { surname: changedStore.surname } : {}),
+                  ...(changedStore.middlename ? { middlename: changedStore.middlename } : {}),
+                  ...(changedStore.rank !== undefined ? { rank: changedStore.rank } : {}),
                 },
               })
             }}
@@ -141,7 +188,7 @@ export default function TutorScreen() {
           >
             <Text className='text-center text-lg font-sans-bold'>Сохранить</Text>
           </Pressable>
-          <Text className='dark:text-neutral-500 text-xs'>{JSON.stringify({ name: changedName, surname: changedSurname, middlename: changedMiddlename, rank: changedRank }, null, 2)}</Text>
+          {/* <Text className='dark:text-neutral-500 text-xs'>{JSON.stringify({ name: changedName, surname: changedSurname, middlename: changedMiddlename, rank: changedRank }, null, 2)}</Text> */}
         </>
       ) : null}
     </ScrollView>
